ee74f300b41bd4dd5b772f4ef7e2058c31999782
nick <smuskin2016@yandex.ru>
2022-06-03 13:52:57 +0300

    linter fix and README.md

--- src/games/cli-calc.js
+++ src/games/cli-calc.js
@@ -1,23 +1,26 @@
 import { mainLogic } from '../index.js';
-    const introduction = 'What is the result of the expression?';
 
-    const arrOfOperands = ['+', '-', '*'];
-    const calculate = (firstNumber, operand, secondNumber) => {
-        switch (operand) {
-            case '+' :
-                return firstNumber + secondNumber;
-            case '-' :
-                return firstNumber - secondNumber; 
-            case '*' :
-                return firstNumber * secondNumber;     
+const introduction = 'What is the result of the expression?';
+
+const arrOfOperands = ['+', '-', '*'];
+const calculate = (firstNumber, operand, secondNumber) => {
+    switch (operand) {
+        case '+' :
+            return firstNumber + secondNumber;
+        case '-' :
+            return firstNumber - secondNumber; 
+        case '*' :
+            return firstNumber * secondNumber;     
         };
-    };    
-        export const content = () => {
-            const firstNumber = Math.floor(Math.random() * (100 - 1)) + 1;
-            const secondNumber = Math.floor(Math.random() * (100 - 1)) + 1;
-            const operand = arrOfOperands[Math.floor(Math.random() * arrOfOperands.length)];
-            const question = `${firstNumber} ${operand} ${secondNumber}`;
-            const rightAnswer = `${calculate(firstNumber, operand, secondNumber)}`;
-            return [question, rightAnswer];
-    };
-    export const currLogic = () => mainLogic(introduction, content);  
+};   
+ 
+export const content = () => {
+    const firstNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+    const secondNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+    const operand = arrOfOperands[Math.floor(Math.random() * arrOfOperands.length)];
+    const question = `${firstNumber} ${operand} ${secondNumber}`;
+    const rightAnswer = `${calculate(firstNumber, operand, secondNumber)}`;
+    return [question, rightAnswer];
+};
+
+export const currLogic = () => mainLogic(introduction, content);  
--- src/games/cli-gcd.js
+++ src/games/cli-gcd.js
@@ -1,15 +1,16 @@
 import { mainLogic } from '../index.js';
- const introduction = 'Find the greatest common divisor of given numbers.';
- const greatestCommonDivisor = (firstNumber, secondNumber) => {
-    if( ! secondNumber) { 
-        return firstNumber;
-      } 
-      else { 
-        return greatestCommonDivisor(secondNumber, firstNumber % secondNumber);  
-      }
+
+const introduction = 'Find the greatest common divisor of given numbers.';
+const greatestCommonDivisor = (firstNumber, secondNumber) => {
+  if( ! secondNumber) {
+    return firstNumber;
+    }
+  else {
+      return greatestCommonDivisor(secondNumber, firstNumber % secondNumber);  
+    }
  };
 
- export const content = () => {
+export const content = () => {
     const firstNumber = Math.floor(Math.random() * (100 - 1)) + 1;
     const secondNumber = Math.floor(Math.random() * (100 - 1)) + 1;
     const question = `${firstNumber} ${secondNumber}`;
@@ -17,4 +18,4 @@ import { mainLogic } from '../index.js';
     return [question, rightAnswer];
 };
 
-    export const currLogic = () => mainLogic(introduction, content);    
+    export const currLogic = () => mainLogic(introduction, content);
