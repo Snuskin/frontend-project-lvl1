9fc75966df554e545acb05d8026a7add4e6a8398
nick <smuskin2016@yandex.ru>
2022-06-07 12:42:34 +0300

    fixed index.js and make-lint.yml

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import readlineSync from 'readline-sync';
 
+const rightAnswersCount = 3;
 export default function mainLogic(description, conductGame) {
-  const rightAnswersCount = 3;
-
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
